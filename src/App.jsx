ada71755ddb93ccc482c53f88d78d16e9d385615
Tihitna21 <menderesimeneh27@gmail.com>
2025-05-20 09:33:59 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import "./App.css"
+
+
+
+
+function App() {
+  
+  return (
+    <>
+    <div className='App'></div>
+      <h1>maryam</h1>
+    </>
+  )
+}
+
+export default App
