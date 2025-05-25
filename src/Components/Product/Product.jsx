f3e88dbafcaf7439b390a81efff24411274cf731
Tihitna21 <menderesimeneh27@gmail.com>
2025-05-25 22:58:49 +0300

    loader

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -2,33 +2,38 @@ import React, { useEffect, useState } from 'react'
 import axios from "axios"
 import ProductCard from './ProductCard'
 import Classes from "./ProductCard.module.css"
+import Loader from '../Loader/Loader'
 
 const Product = () => {
     const[products, setProducts] = useState()
+    const [isLoading, setisLoading] = useState(false)
 
     useEffect(()=>{
     axios.get("https://fakestoreapi.com/products")
     .then((res)=>{
         setProducts(res.data)
+        isLoading(false)
     }).catch((err)=>{
         console.log(err)
+        isLoading(false)
     })
     }, [])
 
   return (
-    <div >
-      <section className={Classes.products__container}>
-        {
-            products?.map((singleProduct)=>{
-                return <ProductCard product={singleProduct} key={singleProduct.id} />
-            })
-        }
-        
-        </section>   
-
-
-    </div>
-  )
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <section className={Classes.products__container}>
+          {products?.map((singleProduct) => {
+            return (
+              <ProductCard product={singleProduct} key={singleProduct.id} />
+            );
+          })}
+        </section>
+      )}
+    </>
+  );
 }
 
 export default Product
--- src/Pages/ProductDetail/ProductDetail.jsx
+++ src/Pages/ProductDetail/ProductDetail.jsx
@@ -5,25 +5,30 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { productUrl } from '../../Api/EndPoint';
 import ProductCard from '../../Components/Product/ProductCard';
+import Loader from '../../Components/Loader/Loader';
 
 const ProductDetail = () => {
-const {productId} = useParams()
 const [product, setProduct] =useState({})
+const [isLoading, setisLoading] = useState(false)
+const { productId } = useParams();
 useEffect(()=>{
+  setisLoading(true)
 axios.get(`${productUrl}/products/${productId}`)
 .then((res) =>{
   setProduct(res.data)
+  setisLoading(false)
 }).catch((err)=>{
   console.log(err)
+  setisLoading(false)
 })
 }, [])
   return (
     <LayOut>
-    <ProductCard
-    product={product}
-    />
-  </LayOut>
-  )
+      {isLoading ?( <Loader />) : (<ProductCard product={product} />)}
+
+
+    </LayOut>
+  );
   
 }
 
