a34fa0623bc25d3a2289c6c9b5b1d7704fb9cc4f
Tihitna21 <menderesimeneh27@gmail.com>
2025-05-25 22:18:23 +0300

    product detail

--- src/Components/Product/ProductCard.jsx
+++ src/Components/Product/ProductCard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Rating from "@mui/material/Rating"
 import CurrencyFormat from '../CurrencyFormat/CurrencyFormat';
 import Classes from "./ProductCard.module.css";
+import { Link } from 'react-router-dom';
 
 
 
@@ -10,9 +11,9 @@ const ProductCard = ({product}) => {
     const { image, title, id, rating, price} =product;
   return (
     <div className={`${Classes.card__container}`}> 
-      <a href="">
+      <Link to={`/products/${id}`}>
         <img src={image} alt="" />
-      </a>
+      </Link>
       <div>
         <h3>{title}</h3>
         <div className={Classes.rating}>
--- src/Pages/ProductDetail/ProductDetail.jsx
+++ src/Pages/ProductDetail/ProductDetail.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Classes from "./ProductDetail.module.css"
 import LayOut from '../../Components/LayOut/LayOut';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { productUrl } from '../../Api/EndPoint';
+import ProductCard from '../../Components/Product/ProductCard';
 
 const ProductDetail = () => {
-  return 
-  <LayOut>
-    <div>ProductDetail</div>
-  </LayOut>;
+const {productId} = useParams()
+const [product, setProduct] =useState({})
+useEffect(()=>{
+axios.get(`${productUrl}/products/${productId}`)
+.then((res) =>{
+  setProduct(res.data)
+}).catch((err)=>{
+  console.log(err)
+})
+}, [])
+  return (
+    <LayOut>
+    <ProductCard
+    product={product}
+    />
+  </LayOut>
+  )
+  
 }
 
 export default ProductDetail
