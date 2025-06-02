203f297498ea7b469f1fff16414d69e61cb1ac44
Tihitna21 <menderesimeneh27@gmail.com>
2025-06-02 17:17:00 +0300

    updated

--- src/Utility/Firebase.js
+++ src/Utility/Firebase.js
@@ -5,13 +5,14 @@ import "firebase/compat/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBHUTU78O30N5SVrqgVquXTOuCE7u1n2kU",
-  authDomain: "clone-87e46.firebaseapp.com",
-  projectId: "clone-87e46",
-  storageBucket: "clone-87e46.firebasestorage.app",
-  messagingSenderId: "697168158403",
-  appId: "1:697168158403:web:4d6b25c92a1fb34d770c76",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
